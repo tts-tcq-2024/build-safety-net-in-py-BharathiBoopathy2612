2ebd315d8fc59adfff78715dbd0067f244a2d6d9
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-07-08 09:43:02 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -1,40 +1,33 @@
-#ifndef SOUNDEX_H
-#define SOUNDEX_H
-
-#include "Soundex.h"
-#include <ctype.h>
-#include <string.h>
-
-char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
+def map_char_to_soundex(c):
+    c = c.upper()
+    soundex_mapping = {
+        'B': '1', 'F': '1', 'P': '1', 'V': '1',
+        'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
+        'D': '3', 'T': '3',
+        'L': '4',
+        'M': '5', 'N': '5',
+        'R': '6'
     }
-}
+    return soundex_mapping.get(c, '0')
 
-void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1;
+def init_soundex_key(name):
+    initial_char = name[0].upper()
+    return initial_char, map_char_to_soundex(initial_char)
 
-    for (int i = 1; i < len && sIndex < 4; i++) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
-        }
-    }
+def valid_soundex_code(char, current_code, previous_code):
+    return current_code != '0' and current_code != previous_code
 
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
-    }
+def compile_soundex(name, soundex_key, previous_code):
+    result = [map_char_to_soundex(char) for char in name[1:] if valid_soundex_code(char, map_char_to_soundex(char), previous_code)]
+    return (soundex_key + ''.join(result)[:3]).ljust(4, '0')
 
-    soundex[4] = '\0';
-}
+def ensure_soundex_length(soundex_key):
+    return soundex_key.ljust(4, '0')
 
-#endif // SOUNDEX_H
+def create_soundex(name):
+    if not name:
+        return ""
+    
+    soundex_key, previous_code = init_soundex_key(name)
+    soundex_key = compile_soundex(name, soundex_key, previous_code)
+    return ensure_soundex_length(soundex_key)
