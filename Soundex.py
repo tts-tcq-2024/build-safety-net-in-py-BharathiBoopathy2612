a698cd1ef396115e97f4bdf64d36981e09a7a769
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-07-08 09:51:09 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -1,6 +1,6 @@
-def map_char_to_soundex(c):
+def char_to_soundex(c):
     c = c.upper()
-    soundex_mapping = {
+    soundex_map = {
         'B': '1', 'F': '1', 'P': '1', 'V': '1',
         'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
         'D': '3', 'T': '3',
@@ -8,26 +8,26 @@ def map_char_to_soundex(c):
         'M': '5', 'N': '5',
         'R': '6'
     }
-    return soundex_mapping.get(c, '0')
+    return soundex_map.get(c, '0')
 
-def init_soundex_key(name):
+def start_soundex_key(name):
     initial_char = name[0].upper()
-    return initial_char, map_char_to_soundex(initial_char)
+    return initial_char, char_to_soundex(initial_char)
 
-def valid_soundex_code(char, current_code, previous_code):
+def valid_code_addition(char, current_code, previous_code):
     return current_code != '0' and current_code != previous_code
 
-def compile_soundex(name, soundex_key, previous_code):
-    result = [map_char_to_soundex(char) for char in name[1:] if valid_soundex_code(char, map_char_to_soundex(char), previous_code)]
-    return (soundex_key + ''.join(result)[:3]).ljust(4, '0')
+def accumulate_soundex(name, soundex_key, previous_code):
+    soundex_codes = [char_to_soundex(char) for char in name[1:] if valid_code_addition(char, char_to_soundex(char), previous_code)]
+    return (soundex_key + ''.join(soundex_codes)[:3]).ljust(4, '0')
 
-def ensure_soundex_length(soundex_key):
+def pad_soundex_key(soundex_key):
     return soundex_key.ljust(4, '0')
 
 def create_soundex(name):
     if not name:
         return ""
-    
-    soundex_key, previous_code = init_soundex_key(name)
-    soundex_key = compile_soundex(name, soundex_key, previous_code)
-    return ensure_soundex_length(soundex_key)
+
+    soundex_key, previous_code = start_soundex_key(name)
+    soundex_key = accumulate_soundex(name, soundex_key, previous_code)
+    return pad_soundex_key(soundex_key)
