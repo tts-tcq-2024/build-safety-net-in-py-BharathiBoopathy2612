297b8d41b477be28753f0b0cba1f91ed27f5bc15
BharathiBoopathy2612 <168723632+BharathiBoopathy2612@users.noreply.github.com>
2024-07-08 10:05:25 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -1,87 +1,93 @@
 import unittest
-from Soundex import create_soundex, char_to_soundex
-
+from Soundex import generate_soundex
+from Soundex import get_soundex_code
+ 
 class TestSoundex(unittest.TestCase):
-
+ 
     def test_empty_string(self):
-        self.assertEqual(create_soundex(""), "")
-
+        self.assertEqual(generate_soundex(""), "")
+ 
     def test_single_character(self):
-        self.assertEqual(create_soundex("M"), "M000")
-
+        self.assertEqual(generate_soundex("M"), "M000")
+ 
     def test_two_characters(self):
-        self.assertEqual(create_soundex("MI"), "M000")
-        self.assertEqual(create_soundex("MB"), "M100")
-
+        self.assertEqual(generate_soundex("MI"), "M000")
+        self.assertEqual(generate_soundex("MB"), "M100")
+ 
     def test_three_characters(self):
-        self.assertEqual(create_soundex("ART"), "A630")
-        self.assertEqual(create_soundex("ABC"), "A120")
-
-    def test_common_name(self):
-        self.assertEqual(create_soundex("Smith"), "S530")
-        self.assertEqual(create_soundex("Johnson"), "J525")
-
-    def test_name_with_vowels(self):
-        self.assertEqual(create_soundex("Aeiou"), "A000")
-        self.assertEqual(create_soundex("AeIou"), "A000")
-
-    def test_name_with_mixed_case(self):
-        self.assertEqual(create_soundex("McDonald"), "M235")
-        self.assertEqual(create_soundex("mcdonald"), "M235")
-
+        self.assertEqual(generate_soundex("ART"), "A630")
+        self.assertEqual(generate_soundex("ABC"), "A120")
+ 
+    def test_name_with_similar_sounding_letters(self):
+        self.assertEqual(generate_soundex("Floor"), "F460")
+        self.assertEqual(generate_soundex("Flower"), "F460")
+    def test_name_with_varying_length(self):
+        # Names shorter than 4 characters
+        self.assertEqual(generate_soundex("Ml"), "M400")
+        self.assertEqual(generate_soundex("Ro"), "R000")
+        self.assertEqual(generate_soundex("Was"), "W200")
+ 
+        self.assertEqual(generate_soundex("Part"), "P630")
+        self.assertEqual(generate_soundex("Rose"), "R200")
+ 
+        # Names longer than 4 characters
+        self.assertEqual(generate_soundex("Random"), "R535")
+        self.assertEqual(generate_soundex("Parent"), "P653")
     def test_name_with_repeating_characters(self):
-        self.assertEqual(create_soundex("Mmmm"), "M000")
-        self.assertEqual(create_soundex("Bbbb"), "B000")
-
+        self.assertEqual(generate_soundex("Maaa"), "M000")
+ 
+ 
     def test_name_with_non_alphabetic_characters(self):
-        self.assertEqual(create_soundex("A1"), "A000")
-        self.assertEqual(create_soundex("B2R"), "B600")
-        self.assertEqual(create_soundex("A!@#$%^&*()"), "A000")
-
+        self.assertEqual(generate_soundex("A1"), "A000")
+        self.assertEqual(generate_soundex("B2R"), "B600")
+        self.assertEqual(generate_soundex("A!@#$%^&*()"), "A000")
+ 
     def test_name_with_numbers_only(self):
-        self.assertEqual(create_soundex("1234"), "1000")
-
-    def test_name_with_special_characters(self):
-        self.assertEqual(create_soundex("O'Malley"), "O540")
-        self.assertEqual(create_soundex("D'Artagnan"), "D635")
-
-    def test_name_with_silent_letters(self):
-        self.assertEqual(create_soundex("Knight"), "K523")
-        self.assertEqual(create_soundex("Wright"), "W623")
-
-    def test_name_with_same_sounding_letters(self):
-        self.assertEqual(create_soundex("Ashcraft"), "A261")
-        self.assertEqual(create_soundex("Tymczak"), "T522")
-
-    def test_char_to_soundex(self):
-        self.assertEqual(char_to_soundex('B'), '1')
-        self.assertEqual(char_to_soundex('F'), '1')
-        self.assertEqual(char_to_soundex('P'), '1')
-        self.assertEqual(char_to_soundex('V'), '1')
-        self.assertEqual(char_to_soundex('C'), '2')
-        self.assertEqual(char_to_soundex('G'), '2')
-        self.assertEqual(char_to_soundex('J'), '2')
-        self.assertEqual(char_to_soundex('K'), '2')
-        self.assertEqual(char_to_soundex('Q'), '2')
-        self.assertEqual(char_to_soundex('S'), '2')
-        self.assertEqual(char_to_soundex('X'), '2')
-        self.assertEqual(char_to_soundex('Z'), '2')
-        self.assertEqual(char_to_soundex('D'), '3')
-        self.assertEqual(char_to_soundex('T'), '3')
-        self.assertEqual(char_to_soundex('L'), '4')
-        self.assertEqual(char_to_soundex('M'), '5')
-        self.assertEqual(char_to_soundex('N'), '5')
-        self.assertEqual(char_to_soundex('R'), '6')
-        self.assertEqual(char_to_soundex('H'), '0')
-        self.assertEqual(char_to_soundex('W'), '0')
-        self.assertEqual(char_to_soundex('A'), '0')
-        self.assertEqual(char_to_soundex('E'), '0')
-        self.assertEqual(char_to_soundex('I'), '0')
-        self.assertEqual(char_to_soundex('O'), '0')
-        self.assertEqual(char_to_soundex('U'), '0')
-        self.assertEqual(char_to_soundex('Y'), '0')
-        self.assertEqual(char_to_soundex('1'), '0')
-        self.assertEqual(char_to_soundex('*'), '0')
-
+        self.assertEqual(generate_soundex("1234"), "1000")
+ 
+    def test_get_soundex_code(self):
+        # Test for '1' mappings
+        self.assertEqual(get_soundex_code('B'), '1')
+        self.assertEqual(get_soundex_code('F'), '1')
+        self.assertEqual(get_soundex_code('P'), '1')
+        self.assertEqual(get_soundex_code('V'), '1')
+        # Test for '2' mappings
+        self.assertEqual(get_soundex_code('C'), '2')
+        self.assertEqual(get_soundex_code('G'), '2')
+        self.assertEqual(get_soundex_code('J'), '2')
+        self.assertEqual(get_soundex_code('K'), '2')
+        self.assertEqual(get_soundex_code('Q'), '2')
+        self.assertEqual(get_soundex_code('S'), '2')
+        self.assertEqual(get_soundex_code('X'), '2')
+        self.assertEqual(get_soundex_code('Z'), '2')
+ 
+        # Test for '3' mappings
+        self.assertEqual(get_soundex_code('D'), '3')
+        self.assertEqual(get_soundex_code('T'), '3')
+ 
+        # Test for '4' mapping
+        self.assertEqual(get_soundex_code('L'), '4')
+ 
+        # Test for '5' mappings
+        self.assertEqual(get_soundex_code('M'), '5')
+        self.assertEqual(get_soundex_code('N'), '5')
+ 
+        # Test for '6' mapping
+        self.assertEqual(get_soundex_code('R'), '6')
+ 
+        # Test for characters that should return '0'
+        self.assertEqual(get_soundex_code('H'), '0')
+        self.assertEqual(get_soundex_code('W'), '0')
+        self.assertEqual(get_soundex_code('A'), '0')
+        self.assertEqual(get_soundex_code('E'), '0')
+        self.assertEqual(get_soundex_code('I'), '0')
+        self.assertEqual(get_soundex_code('O'), '0')
+        self.assertEqual(get_soundex_code('U'), '0')
+        self.assertEqual(get_soundex_code('Y'), '0')
+        self.assertEqual(get_soundex_code('1'), '0')
+        self.assertEqual(get_soundex_code('*'), '0')
+ 
+ 
+ 
 if __name__ == '__main__':
     unittest.main()
